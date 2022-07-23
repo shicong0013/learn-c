ff66496b9299a9d15af34cb6a4ca75536ee20130
Cong <1012296791@qq.com>
2022-07-23 14:47:52 +0800

    bootstrap-table

---
+++ js/boostrap-tbale.js
@@ -0,0 +1,17 @@
+function getValue () {
+  var item = [];
+  var newValue = document.getElementsByTagName("input");
+  for (var i = 0; i < newValue.length; i++) {
+    if (newValue[i].checked) {
+      item.push(newValue[i].value);
+    }
+  }
+  return item;
+}
+
+function setClass () {
+  var classArr = getValue();
+  classArr = classArr.join(" ");
+  var table = document.getElementsByTagName("table")[0];
+  table.setAttribute("class", classArr);
+}
