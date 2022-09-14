3ab030625b988f09b5f68ad9a016b4339b10a236
Cong <1012296791@qq.com>
2022-09-14 20:49:54 +0800

    index

--- lodash/shicong0013-lodash.js
+++ lodash/shicong0013-lodash.js
@@ -37,5 +37,40 @@ var shicong0013 = {
       }
     }
     return result;
-  }
+  },
+  differenceBy: function (array, ...values) {
+    let diff = values;
+    let result = [];
+    //当数组的最后一个参数位数组时说明没有传入检查元素的函数
+    if (!Array.isArray(values.at(-1))) {
+      diff = values.slice(0, -1);
+      let f = values.slice(-1);
+    }
+    //展平数组
+    diff = diff.flat();
+    if (typeof f == 'function') {
+      let temp = diff.every(x => f(x) != f(array[i]));
+      if (temp) {
+        result.push(array[i]);
+      }
+    } else {
+      result = shicong0013.difference(array, diff);
+    }
+    return result;
+  },
+  drop: function () { },
+  dropWhile: function () { },
+  dropRight: function () { },
+  dropRightWhile: function () { },
+  fill: function () { },
+  findIndex: function () { },
+  findLastIndex: function () { },
+  flatten: function () { },
+  flattenDeep: function () { },
+  flattenDepth: function () { },
+  fromPairs: function () { },
+  head: function () { },
+  indexOf: function () { },
+  initial: function () { },
+  intersection: function () { },
 }
