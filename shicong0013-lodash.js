0c14586319a3e0c93ce922a7bc58e12f33a4ac48
Cong <1012296791@qq.com>
2022-09-14 19:49:12 +0800

    difference

--- shicong0013-lodash.js
+++ shicong0013-lodash.js
@@ -3,11 +3,39 @@ var shicong0013 = {
     var arr = []
     for (var i = 0; i < array.length;) {
       let temp = [];
-      for (var j = 0; j < size; j++, i++) {
+      for (var j = 0; j < size && i < array.length; j++, i++) {
         temp.push(array[i]);
       }
       arr.push(temp);
     }
     return arr;
   },
+  compact: function (array) {
+    var result = [];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i]);
+      }
+    }
+    return result;
+  },
+  difference: function (array, ...values) {
+    var result = [];
+    var diff = [];
+    if (values.length > 1) {
+      for (var k = 0; k < values.length; k++) {
+        diff.push(...values[k]);
+      }
+    } else {
+      diff.push(...values[0]);
+    }
+    for (var i = 0; i < array.length; i++) {
+      //result.push(diff.every((element => element != array[i])));
+      let temp = diff.every(x => x != array[i]);
+      if (temp) {
+        result.push(array[i]);
+      }
+    }
+    return result;
+  }
 }
